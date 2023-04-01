1654e77aa3df70e3cdd155b8d42763ab39e69bda
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2023-04-01 16:56:35 -0500

    Entity Tickets created

---
+++ TicketSystem/DAL/Entities/Tickets.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketSystem.DAL.Entities
+{
+    public class Tickets
+    {
+        #region Properties
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+        [Display (Name = "Fecha de uso de la boleta")]
+        public DateTime UseDate { get; set; }
+        [Display (Name = "Estado de la boleta")]
+        public bool IsUsed{ get; set; }
+        [Display (Name = "Puerta de entrada")]
+        public string EntranceGate{ get; set; }
+
+
+        #endregion
+    }
+}
